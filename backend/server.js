8f9035d6ff8bc7e398867dcb381a6d9053a49f2c
Priyanka Patel <ppriyankapatel58@gmail.com>
2026-03-25 17:00:40 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -100,6 +100,6 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
-})
+});
